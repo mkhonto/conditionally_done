74ac925b554b0242f311d855004d4e45c6029e7d
mkhonto <jabulani@projectcodex.co>
2016-11-16 14:27:11 +0200

    if else statement executing the right block

---
+++ if_tuesday.js
@@ -0,0 +1,12 @@
+var month = "June";
+//evaluate it to true
+var isJune = month === "July";
+
+if(isJune){
+  //run this code if june
+  console.log("By, by winter rains June!");
+}
+else{
+  //run this code if not June
+  console.log("cool days : " + month);
+}
