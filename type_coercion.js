b7f935d776fd2f4df1468357a2200e726720410e
mkhonto <jabulani@projectcodex.co>
2016-11-17 10:36:38 +0200

    converting the string into a number

--- type_coercion.js
+++ type_coercion.js
@@ -1,7 +1,9 @@
 const assert = require("assert");
 //this will evaluate to true
 var match = ("190" == 190);
+console.log(match);
 assert.equal(match, true);
 
 var match = "190" === 190;
+console.log(match);
 assert.equal(match, false);
